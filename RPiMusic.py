1ea9c0af1800a930d162d6489307cb2bd9d451a8
Lara Maia <lara@craft.net.br>
2014-06-19 17:09:29 -0300

    Add setup.py

--- RPiMusic.py
+++ RPiMusic.py
@@ -3,7 +3,7 @@
 #
 #    Copyright (C) 2014 Lara Maia <lara@craft.net.br>
 #
-#    spkMusic is free software: you can redistribute it and/or modify
+#    RPiMusic is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#
+#    Copyright (C) 2014 Lara Maia <lara@craft.net.br>
+#
+#    This file is part of RPiMusic.
+#
+#    RPiMusic is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    http://www.gnu.org/licenses/gpl.html
+#
+
+from distutils.core import setup
+
+setup(name='RPiMusic',
+      version='1.0',
+      description='RPiMusic module for python 3',
+      author='Lara Maia',
+      author_email='lara@craft.net.br',
+      url='lara.craft.net.br',
+      py_modules=['RPiMusic']
+     )
+
